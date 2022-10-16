d5f16d592a2e642aa2b6cde4ec2ac4ad68799f2f
mkazu0428 <k.miyahara@4nonome.com>
2022-10-16 21:20:41 +0900

    section4の完了

--- section/section4/section4.js
+++ section/section4/section4.js
@@ -1,5 +1,11 @@
-const echoDiv = null;
+const echoDiv = function (x, y) {
+  const remainder = x % y;
+  const quotient = (x - remainder) / y;
+  return "商: " + String(quotient) + ", 余り: " + String(remainder);
+};
 
-const reply = null;
+const reply = function (z = "返信なし") {
+  return z;
+};
 
 module.exports = { echoDiv, reply };
