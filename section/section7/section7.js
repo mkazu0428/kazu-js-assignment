8517b52688a25c371ee5425baa79b871d97a3ad1
mkazu0428 <k.miyahara@4nonome.com>
2022-10-18 00:34:50 +0900

    section7の完了

--- section/section7/section7.js
+++ section/section7/section7.js
@@ -1,9 +1,19 @@
-const character = null;
+const character = { id: 1, name: "スライム", hp: 50 };
 
-const addMP = null;
+const addMP = (x) => {
+  character.mp = x;
+  return character;
+};
 
-const updateHP = null;
+const updateHP = () => {
+  const h = character.hp;
+  character.hp = h - 15;
+  delete character.mp;
+  return character;
+};
 
-const LookUpName = null;
+const LookUpName = () => {
+  return character.name;
+};
 
 module.exports = { character, addMP, updateHP, LookUpName };
