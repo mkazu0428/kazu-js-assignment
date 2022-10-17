e3ee5fc19165c273e6607e066872cedf9899f28a
mkazu0428 <k.miyahara@4nonome.com>
2022-10-17 18:17:04 +0900

    section6の完了

--- section/section6/section6.js
+++ section/section6/section6.js
@@ -1,3 +1,11 @@
-const sum = null;
+const sum = (a, b) => {
+  let x = Math.min(a, b);
+  let y = Math.max(a, b);
+  let result = 0;
+  for (let i = x; i <= y; i++) {
+    result = result + i;
+  }
+  return result;
+};
 
 module.exports = { sum };
