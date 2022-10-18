3e28e5c95d8613d560965c479cdf8a6dea2e54a6
mkazu0428 <k.miyahara@4nonome.com>
2022-10-18 18:24:21 +0900

    section8の完了

--- section/section8/section8.js
+++ section/section8/section8.js
@@ -1,13 +1,44 @@
-const firstIndex = null;
-const countArrayLength = null;
+const firstIndex = (array) => {
+  return array[0];
+};
+const countArrayLength = (array) => {
+  if (Array.isArray(array)) {
+    if (array.length === 0) {
+      return "この配列は空配列です";
+    } else {
+      return `この配列は${array.length}個のデータを持っています`;
+    }
+  } else {
+    return "これは配列ではありません";
+  }
+};
 
-const extractArray = null;
+const extractArray = (array) => {
+  return array.slice(1, 3);
+};
 
-const makeMark = null;
+const makeMark = (mark, number) => {
+  const newArray = [];
+  for (let i = 0; i < number; i++) {
+    newArray.push(mark);
+  }
+  return newArray;
+};
 
-const onlyString = null;
+const onlyString = (array) => {
+  const stringArray = array.filter((currentValue, index, array) => {
+    return typeof currentValue === "string";
+  });
+  return stringArray;
+};
 
-const addNumber = null;
+const addNumber = (array) => {
+  numberAddedArray = [];
+  array.forEach((currentValue, index, array) => {
+    numberAddedArray.push(`${index + 1}: ${currentValue}`);
+  });
+  return numberAddedArray;
+};
 
 module.exports = {
   firstIndex,
